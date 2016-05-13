2dcd88ce767c06aba27cd307f0cbd8e32d2d4804
tazam <tazam@Tazam-lin>
2016-05-13 17:02:57 +0200

    Methode pour la direction des bonus à testé.

--- src/fr/univavignon/courbes/agents/voisin/AgentImpl.java
+++ src/fr/univavignon/courbes/agents/voisin/AgentImpl.java
@@ -290,10 +290,11 @@ public class AgentImpl extends Agent
 /**
 * Choisi un bonus.
 * @param var contient un int qui influ sur la note suivant la situation du snake.
-* @return un tableau d'int qui va contenir les coordonées des bonus x en [0][] r=et y en [1][];
+* @return un tableau d'int qui va contenir les coordonées des bonus x en [0][] r=et y en [1][]; si le nombre de bonus est de zero, resutl[0][0]==-1
+* sinon resutl[0][0] contien la taille;
 */
 
-private int[][] processBonus(int var)
+private int[][] processBonus()
 {
 	checkInterruption();	// on doit tester l'interruption au début de chaque méthode
 
@@ -306,8 +307,17 @@ private int[][] processBonus(int var)
 		k++;
 	}
 	// va contenir les coordonées des bonus x en [0][] r=et y en [1][]; une note sera attribué en [2][]
-	int result[][]= new int[3][k];
-	k=0;
+	int result[][]= new int[3][k+1];
+	// si il n'y à pas de bonus
+	if(k==0)
+	{
+		result[0][0]=-1;
+		return result;
+	}else
+	{
+		result[0][0]=k+1;
+	}
+	k=1;
 
 
 
@@ -320,32 +330,29 @@ private int[][] processBonus(int var)
 		// on attribue une note selon le type de bonus
 		switch(i.type)
 		{	case OTHERS_FAST:
-			result[2][k]=1;
+			result[2][k]=-1;
 			break;
 		case OTHERS_REVERSE:
-			result[2][k]=500;
+			result[2][k]=-5;
 			break;
 		case OTHERS_THICK:
-			result[2][k]=3;
+			result[2][k]=-2;
 			break;
 		case OTHERS_SLOW:
-			result[2][k]=400;
+			result[2][k]=-4;
 			break;
 		case USER_FAST:
-			result[2][k]=5;
+			result[2][k]=-3;
 			break;
 		case USER_FLY:
-			result[2][k]=500;
+			result[2][k]=5;
 			break;
 		case USER_SLOW:
-			result[2][k]=10;
+			result[2][k]=1;
 			break;
 		}
 		// la note est modifier selon la distance entre les differents serpent et le bonus.
-		if(horsPortee(i))
-		{
-			result[2][k]+=-1000;
-		}
+		
 		
 		
 		k++;
@@ -355,13 +362,85 @@ private int[][] processBonus(int var)
 
 }
 
-	/**
-	 * @param i est l'item que l'on test
-	 * @return true si l'agent ne peut pas atteindre le bonus.
-	 */
-	private boolean horsPortee(ItemInstance i)
-	{
-		return true;
-	}
+/**
+ * @param var true si on veut un bonus defensif false pour un offensif
+ * @return 1 si le meilleur bonus est à gauche, 2 s'il est à droite, 0 si la direction est bonne.
+ */
+private int BonusDirection(boolean var)
+{
+	int bonus[][]=processBonus();
 	
+	if(bonus[0][0]==-1)
+	{
+		return 0;
+	}
+	int k=1;
+	// si on veu un bonus defensif
+	if (var)
+	{
+		int max=bonus[2][1];
+		
+		// on choisi le meilleur bonus;
+		for(int i=1;i<bonus[0][0]-1;i++)
+		{
+			checkInterruption();	// une boucle, donc un autre test d'interruption
+			if(max<bonus[2][i])
+			{
+				max=bonus[2][i];
+				k=i;
+			}
+		}
+	}else
+	{
+		int min=bonus[2][1];
+		
+		// on choisi le meilleur bonus;
+		for(int i=1;i<bonus[0][0]-1;i++)
+		{
+			checkInterruption();	// une boucle, donc un autre test d'interruption
+			if(min>bonus[2][i])
+			{
+				min=bonus[2][i];
+				k=i;
+			}
+		}
+	}
+	Position position = new Position(bonus[0][k],bonus[1][k]);
+	// on calcul l'angle entre la tete du de l'agent et le bonus
+	double angle = Math.atan2(position.y-agentSnake.currentY, position.x-agentSnake.currentX);
+	if(angle<0)
+	{
+		angle = angle + 2*Math.PI;	
+	}
+
+	if(angle==0)// le bonus est devant
+		{
+			return 0;
+		}
+		
+		if(angle==Math.PI)// si le bonus est derrière (à amélioré)
+		{
+			return 1;
+		}
+		
+		if(angle<Math.PI)// le bonus est à gauche
+		{
+			return 1;
+		}else
+			if (angle<2*Math.PI)// le bonus est à droite.
+			{
+				return 2;
+			}
+		
+		return 0;
+	}
+
+
+
+
+
+
+
+
 }
+
